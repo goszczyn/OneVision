e2d2c73db4858a8991f6e4c203e27d8bca9f2b2b
goszczyn <goszczyn@gmail.com>
2017-11-11 19:29:37 +0100

    parent

--- Readme.txt
+++ Readme.txt
@@ -9,3 +9,6 @@ docker-compose -f config-server-compose.yml up
 1. Oracle JCE
 2. Generate key
 keytool -genkeypair -alias cfgServerKey -keyalg RSA -dname "CN=Config Server,OU=Unit,O=Goszczynofabryka,L=Dziekanow,S=Mazowieckie,C=PL" -keypass 1qaz2wsx -keystore server.jks -storepass passwor123 -validity 365
+
+
+docker exec -t -i docker_eurekaserver_1 sh
---
+++ setEnv.bat
@@ -0,0 +1,7 @@
+set DOCKER_TLS_VERIFY=1
+set DOCKER_HOST=tcp://192.168.99.102:2376
+set DOCKER_CERT_PATH=C:\Users\goszczyn\.docker\machine\machines\default
+set DOCKER_MACHINE_NAME=default
+set COMPOSE_CONVERT_WINDOWS_PATHS=true
+
+set PATH=C:\Windows\system32;C:\Windows;D:\work\apache-maven-3.5.0\bin;D:\work\Java\jdk1.8.0_144\bin;C:\Program Files\Docker Toolbox
