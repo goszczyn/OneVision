8d0eb56f784d44b49e3d2253686d78ebd8e4a2a8
goszczyn <goszczyn@gmail.com>
2017-11-11 19:28:59 +0100

    docker

---
+++ docker/setEnv.bat
@@ -0,0 +1,8 @@
+set DOCKER_TLS_VERIFY=yes
+set DOCKER_IP=192.168.99.102
+set DOCKER_HOST=tcp://%DOCKER_IP%:2376
+set DOCKER_CERT_PATH=C:\Users\goszczyn\.docker\machine\machines\default
+set DOCKER_MACHINE_NAME=default
+set COMPOSE_CONVERT_WINDOWS_PATHS=true
+
+set PATH=C:\Windows\system32;C:\Windows;C:\work\apache-maven-3.5.0\bin;c:\work\Java\jdk1.8.0_144\bin;C:\Program Files\Docker Toolbox
