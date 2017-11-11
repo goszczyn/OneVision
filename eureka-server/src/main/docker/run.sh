ba9cd4a9d91c0ccede799d215c40fc15da175a0a
goszczyn <goszczyn@gmail.com>
2017-11-11 19:14:45 +0100

    eureka

--- eureka-server/src/main/docker/run.sh
+++ eureka-server/src/main/docker/run.sh
@@ -4,5 +4,7 @@ echo "Starting the Eureka Server"
 echo "********************************************************"
 java -Djava.security.egd=file:/dev/./urandom 					          \
      -Deureka.serviceUrl.defaultZone=$EUREKASERVER_URI            		  \
+     -Dgraylog.host=$GRAYLOG_HOST                                         \
+     -Dgraylog.port=$GRAYLOG_PORT                                         \
      -jar /usr/local/eureka-server/@project.build.finalName@.jar
 	 
